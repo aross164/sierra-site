7e9f681ad3ccafe4f31c9ca0b8f1b755988add67
alex <aross16412@gmail.com>
2022-10-01 17:04:00 -0500

    Add redirect file, add all routes

--- src/index.js
+++ src/index.js
@@ -39,6 +39,50 @@ const router = createBrowserRouter([
                 path: 'dance',
                 element: <EditRankings week={4}/>
             },
+            {
+                path: 'eager',
+                element: <EditRankings week={5}/>
+            },
+            {
+                path: 'face',
+                element: <EditRankings week={6}/>
+            },
+            {
+                path: 'girl',
+                element: <EditRankings week={7}/>
+            },
+            {
+                path: 'hamper',
+                element: <EditRankings week={8}/>
+            },
+            {
+                path: 'idle',
+                element: <EditRankings week={9}/>
+            },
+            {
+                path: 'jumper',
+                element: <EditRankings week={10}/>
+            },
+            {
+                path: 'kid',
+                element: <EditRankings week={11}/>
+            },
+            {
+                path: 'looker',
+                element: <EditRankings week={12}/>
+            },
+            {
+                path: 'mount',
+                element: <EditRankings week={13}/>
+            },
+            {
+                path: 'nail',
+                element: <EditRankings week={14}/>
+            },
+            {
+                path: 'oasis',
+                element: <EditRankings week={15}/>
+            },
         ]
     }
 ]);
